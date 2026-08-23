15182d1df8070b75ce24a350783286a667ab31e9
Jompas19 <joaopaulo191102gpt@gmail.com>
2026-08-23 10:49:55 -0300

    Carregar organização em pastas e estudo combinado

--- public/js/study.js
+++ public/js/study.js
@@ -1,7 +1,6 @@
 (function () {
   function loadModule(src, dataAttribute, errorMessage) {
     if (document.querySelector(`script[${dataAttribute}]`)) return;
-
     const script = document.createElement("script");
     script.async = false;
     script.src = src;
@@ -17,6 +16,12 @@
   );
 
   loadModule(
+    "js/multi-study.js",
+    "data-oitucards-multi-study",
+    "Não foi possível carregar o estudo combinado."
+  );
+
+  loadModule(
     "js/import-anki-compat.js",
     "data-oitucards-import-anki-compat",
     "Não foi possível carregar a camada de compatibilidade com o Anki."
@@ -26,5 +31,11 @@
     "js/import.js",
     "data-oitucards-import",
     "Não foi possível carregar o importador de baralhos."
+  );
+
+  loadModule(
+    "js/library.js",
+    "data-oitucards-library",
+    "Não foi possível carregar a organização da biblioteca."
   );
 })();
