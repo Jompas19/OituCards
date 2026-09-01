1e09c162aaf8a6d4c9c5d35db2fe8c483f250366
Jompas19 <joaopaulo191102gpt@gmail.com>
2026-09-01 15:21:38 -0300

    Carregar experiência modal da preparação de estudo

--- public/js/multi-study-guard.js
+++ public/js/multi-study-guard.js
@@ -46,6 +46,15 @@
     return script;
   }
 
+  function loadStudyConfigModalUx() {
+    loadScript(
+      'script[data-oitucards-study-config-modal-ux]',
+      "js/study-config-modal-ux.js?v=20260901-1525",
+      "oitucardsStudyConfigModalUx",
+      "Não foi possível carregar o refinamento da preparação de estudo."
+    );
+  }
+
   function loadLibraryPerformance() {
     loadScript('script[data-oitucards-library-performance]', "js/library-performance.js?v=20260823-1140", "oitucardsLibraryPerformance", "Não foi possível carregar as otimizações da biblioteca.");
   }
@@ -134,6 +143,7 @@
   installImmediateExactDueRead();
   document.documentElement.classList.add("review-due-ready");
 
+  loadStudyConfigModalUx();
   loadLibraryPerformance();
   loadLibraryStability();
   loadStudyExitFlow();
